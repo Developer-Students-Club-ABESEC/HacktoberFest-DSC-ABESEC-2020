16ce7058f95aa457f73f13891c7fdede00686a90
Shrutiagarwar <44864936+Shrutiagarwar@users.noreply.github.com>
2020-10-30 01:17:54 +0530

    Create armstrong_range.py

---
+++ Python/armstrong_range.py
@@ -0,0 +1,20 @@
+#This program run  within the range
+lower = int(input("enter the starting point "))
+upper = int(input("Enter the ending point "))
+
+for num in range(lower, upper + 1):
+
+   # order of number
+   order = len(str(num))
+    
+   # initialize sum
+   sum = 0
+
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)

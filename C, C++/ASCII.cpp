de18a59c0f5dad5e02c3014c32179f16c45d7872
Utkarsh Srivastava <65492989+utkarsh7598@users.noreply.github.com>
2020-10-29 19:24:35 +0530

    create ASCII.cpp

---
+++ C, C++/ASCII.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    int value;
+    cout<<"Enter the character:";
+    cin>>ch;
+    value=ch;
+    cout<<"The ASCII value:"<<value;
+    return 0;
+}

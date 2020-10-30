5628fa5b282b3be40097b806c2f683bbc2103c5b
Utkarsh Srivastava <65492989+utkarsh7598@users.noreply.github.com>
2020-10-30 13:11:08 +0530

    Create Rectanglepattern.cpp

---
+++ C, C++/Rectanglepattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{int row,col;
+    cout<<"Enter row and column";
+ cin>>row>>col;
+        for(int i=1;i<=row;i++)
+       {
+          for(int j=1;j<=col;j++)
+          {
+              cout<<"* ";
+          }cout<<endl;
+       }
+    return 0;
+}

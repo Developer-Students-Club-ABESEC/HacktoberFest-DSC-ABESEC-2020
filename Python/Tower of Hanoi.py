844c13208f03d31bae2d28f0008e98083417fb11
singhal-abhi <61381426+singhal-abhi@users.noreply.github.com>
2020-10-31 08:15:35 +0530

    Update Tower of Hanoi.py
    value of n is now user-defined

--- Python/Tower of Hanoi.py
+++ Python/Tower of Hanoi.py
@@ -9,7 +9,7 @@ def TowerOfHanoi(n , from_rod, to_rod, aux_rod):
 	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod) 
 		
 # Driver code 
-n = 4
+n =int(input("enter no of rings"))	     
 TowerOfHanoi(n, 'A', 'C', 'B') 
 # A, C, B are the name of rods 
 

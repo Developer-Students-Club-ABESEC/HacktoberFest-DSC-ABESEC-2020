431936eb875beba06f0395db5b05fd9265e983fd
Sanchi Pahuja <73716620+Sanchi2401@users.noreply.github.com>
2020-10-31 22:45:04 +0530

    natural number pattern.cpp

---
+++ C, C++/natural number pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    int count=1;
+    cout<<"\n Enter the value of n:";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
